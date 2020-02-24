d0802075f7554e8470760d61e8456553977d31bb
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-02-24 13:28:05 -1000

    Created AppBuildOne Kat and Isaac

---
+++ appBuildOne/appBuildOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appBuildOne
+//
+//  Created by Isaac Cruz on 2/24/20.
+//  Copyright © 2020 Isaac Cruz. All rights reserved.
+//created by kat and isaac
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
