ca1d5003fff37f10d64a1a8605d7fd01e08b539d
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-02-24 14:46:03 -1000

    partially completed, view controllers established must link bring from tab controller

--- appBuildOne/appBuildOne/ViewController.swift
+++ appBuildOne/appBuildOne/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        @IBOutlet weak var descriptionLabel: UILabel!
+    
+        @IBOutlet weak var firstTabLabel: UILabel!
+    
+        @IBOutlet weak var uhwoLabel: UILabel!
+    
+    
+        @IBOutlet weak var aboutAcmChangeButtonPressed: UIButton!
+    
+    
+override func viewDidLoad()
+{
+        self.firstTabLabel.text = "University of Hawaii West Oahu"
+        self.view.backgroundColor =  UIColor.gray
+        self.descriptionLabel.text = "Since its inception in 1976, the University of Hawaiʻi – West Oʻahu has been committed to delivering accessible, affordable higher education to residents on the Leeward Coast through dynamic, flexible, and responsive methods."
+        
+        
+super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
